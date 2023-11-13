8201d95b0cf45d763d2653aa6b0032354e9874d2
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-14 01:33:18 +0600

    add usestate for active

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {}
 
 const Header = (props: Props) => {
+   const [active, setActive] = useState(false)
   return (
-    <div className={`w-full p-5 border-b min-h-[60px] border-b-[#ffffff32]`}>
+    <div className={`w-full p-5 border-b min-h-[60px] border-b-[#ffffff32] transition-opacity ${active && 'fixed top-0 left-0 bg-[#00000] z-[9999]'}`}>
 
     </div>
   )
