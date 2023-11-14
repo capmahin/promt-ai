20a784d480405e3d5a0e2f368a27294b94d6b452
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-14 12:49:28 +0600

    add navigation formet

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,6 +4,29 @@ type Props = {
     activeItem: number;
 }
 
+const navItems=[
+    {
+        title:"Home",
+        href:"/",
+    },
+    {
+        title:"About Us",
+        href:"/about",
+    },
+    {
+        title:"Marketplace",
+        href:"/marketplace",
+    },
+    {
+        title:"Contact Us",
+        href:"/contact",
+    },
+    {
+        title:"Policy",
+        href:"/policy",
+    }
+]
+
 const Navigation = (props: Props) => {
   return (
     <div>Navigation</div>
