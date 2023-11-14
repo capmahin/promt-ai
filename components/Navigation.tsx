b0404917c7e9bebe013642735722eef0c5cd3690
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-14 22:58:40 +0600

    add color

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -31,9 +31,9 @@ const Navigation = ({activeItem}: Props) => {
   return (
     <div className='block md:flex'>
         {
-            navItems.map((item)=>(
+            navItems.map((item,index)=>(
                 <div key={item.title}>
-                    <h5 className={`inline-block md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500] font-Inter ${activeItem}`}>
+                    <h5 className={`inline-block md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500] font-Inter ${activeItem === index && 'text-[#6dff4b]'}`}>
                         {item.title}
                     </h5>
                 </div>
