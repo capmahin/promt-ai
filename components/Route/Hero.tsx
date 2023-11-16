803db602bc2705f7d6e46e7f4ff0e0abece11482
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-16 13:58:08 +0600

    add

--- components/Route/Hero.tsx
+++ components/Route/Hero.tsx
@@ -5,7 +5,11 @@ type Props = {}
 const Hero = (props: Props) => {
   return (
     <div className='w-full md:min-h-screen flex items-center justify-between'>
-
+     <div>
+        <h1>
+            Make <span>Ai Image</span>
+        </h1>
+     </div>
     </div>
   )
 }
