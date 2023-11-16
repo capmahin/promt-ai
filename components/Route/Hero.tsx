0cc541df7ee096acb4045b6156f43b769fbaaf87
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-16 14:33:24 +0600

    more classes add on h1

--- components/Route/Hero.tsx
+++ components/Route/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = (props: Props) => {
   return (
     <div className='w-full md:min-h-screen flex items-center justify-between'>
      <div>
-        <h1 className='font-Monserrat text-4xl py-5 xl:text-7xl 2xl:text-8xl font-[700] text-center xl:leading-[80px]'>
+        <h1 className='font-Monserrat text-4xl py-5 xl:text-7xl 2xl:text-8xl font-[700] text-center xl:leading-[80px] 2xl:leading-[100px] sm:mt-20'>
             Make <span>Ai Image</span> <br />
             with your <br /> Imagination
         </h1>
