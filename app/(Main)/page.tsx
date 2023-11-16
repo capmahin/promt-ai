b6d8ddb1f0db09a3c24deb379a9a79580d95d707
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-16 14:40:05 +0600

    add state

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,10 +1,18 @@
 'use client'
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import Header from "@/components/Header";
 import Hero from '@/components/Route/Hero';
 type Props = {}
 
 const Page = (props: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    if(!isMounted){
+      setIsMounted(true);
+    }
+  }, [])
+  
   return (
     <div>
       <Header activeItem={0} />
--- components/Route/Hero.tsx
+++ components/Route/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = (props: Props) => {
     <div className='w-full md:min-h-screen flex items-center justify-between'>
      <div>
         <h1 className='font-Monserrat text-4xl py-5 xl:text-7xl 2xl:text-8xl font-[700] text-center xl:leading-[80px] 2xl:leading-[100px] sm:mt-20'>
-            Make <span>Ai Image</span> <br />
+            Make <span className='text-[#64ff4b]'>Ai Image</span> <br />
             with your <br /> Imagination
         </h1>
      </div>
