3d7804306970aa96d03438899302ebcc7cfe5a17
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-18 00:39:52 +0600

    add another div

--- components/Route/About.tsx
+++ components/Route/About.tsx
@@ -5,7 +5,9 @@ type Props = {}
 const About = (props: Props) => {
   return (
     <div className='w-full relative grid md:grid-cols-2 md:py-8'>
+           <div className='col-span-1 w-full md:w-[60%] md:mt-5 px-5 md:px-[unset]'>
 
+           </div>
     </div>
   )
 }
