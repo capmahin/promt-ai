08e6e3895b853f73b184d2929fc44a5a5ed7b927
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-17 22:14:44 +0600

    add

--- components/Route/Hero.tsx
+++ components/Route/Hero.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Hero = (props: Props) => {
   return (
-    <div className='w-full md:min-h-screen flex items-center justify-between'>
+    <div className='w-full md:min-h-screen flex items-center justify-center'>
      <div>
         <h1 className='font-Monserrat text-4xl py-5 xl:text-7xl 2xl:text-8xl font-[700] text-center xl:leading-[80px] 2xl:leading-[100px] sm:mt-20'>
             Make <span className='text-[#64ff4b]'>Ai Image</span> <br />
