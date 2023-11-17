3387b213cc4b5d79e4349861f1caae9a5ad0da2f
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-18 00:57:17 +0600

    more options

--- components/Route/About.tsx
+++ components/Route/About.tsx
@@ -8,8 +8,8 @@ const About = (props: Props) => {
   return (
     <div className='w-full relative grid md:grid-cols-2 md:py-8'>
            <div className='col-span-1 w-full md:w-[60%] md:mt-5 px-5 md:px-[unset]'>
-             <Chip className={`${styles.button} mb-[30px] h-[37px] bg-[#122211f]`}>
-
+             <Chip className={`${styles.button} mb-[30px] h-[37px] bg-[#12211f]`}>
+                  AI Image  
              </Chip>
            </div>
     </div>
