d82e72d6f2d579784b29967c8ae6e53f8e8a0abe
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-18 00:49:26 +0600

    import styles

--- components/Route/About.tsx
+++ components/Route/About.tsx
@@ -1,3 +1,4 @@
+import { styles } from '@/utils/styles'
 import { Chip } from '@nextui-org/react'
 import React from 'react'
 
@@ -7,8 +8,8 @@ const About = (props: Props) => {
   return (
     <div className='w-full relative grid md:grid-cols-2 md:py-8'>
            <div className='col-span-1 w-full md:w-[60%] md:mt-5 px-5 md:px-[unset]'>
-             <Chip>
-                
+             <Chip className={`${styles}`}>
+
              </Chip>
            </div>
     </div>
