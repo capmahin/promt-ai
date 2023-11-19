cafb533cd9021a1067caca8d4870d5768f2f9d19
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-19 11:11:27 +0600

    add another div in card

--- components/Prompts/PromptCard.tsx
+++ components/Prompts/PromptCard.tsx
@@ -6,7 +6,9 @@ type Props = {}
 const PromptCard = (props: Props) => {
   return (
     <Card radius='lg' className='w-full md:w-[31%] 2xl:w-[23%] p-4 bg-[#130f23]'>
+      <div className='relative'>
 
+      </div>
     </Card>
   )
 }
