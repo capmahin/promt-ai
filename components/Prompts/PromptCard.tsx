46169e6d0808c3dec544a9882657b45541e3c07e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-19 11:15:04 +0600

    add images

--- components/Prompts/PromptCard.tsx
+++ components/Prompts/PromptCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@nextui-org/react'
+import { Card, Image } from '@nextui-org/react'
 import React from 'react'
 
 type Props = {}
@@ -7,7 +7,10 @@ const PromptCard = (props: Props) => {
   return (
     <Card radius='lg' className='w-full md:w-[31%] 2xl:w-[23%] p-4 bg-[#130f23]'>
       <div className='relative'>
-
+           <Image 
+           src='https://pixner.net/aikeu/assets/images/category/fourteen.png'
+            alt=''
+           />
       </div>
     </Card>
   )
