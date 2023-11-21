22993f6957713f3c35ca0547334a5f6407fd88f6
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-21 11:17:19 +0600

    add more classes

--- components/Prompts/PromptCard.tsx
+++ components/Prompts/PromptCard.tsx
@@ -54,7 +54,7 @@ const PromptCard = (props: Props) => {
       </div>
       <br />
       <Link href='/shop/123' className='w-full'>
-           <Button className={'mb-3 w-full bg-transparent border border-[#16c252] hover:bg-[#16c252]'}>
+           <Button className={'mb-3 w-full bg-transparent border border-[#16c252] hover:bg-[#16c252] hover:text-black duration-300 transition-opacity font-Inter font-[600]'}>
 
            </Button>
       </Link>
