c9d38613ffec174d51fe6ee4aabd1e325c546b25
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-21 22:09:44 +0600

    add another classnmae

--- components/Shop/SellerCard.tsx
+++ components/Shop/SellerCard.tsx
@@ -8,6 +8,7 @@ const SellerCard = (props: Props) => {
     <Card className='py-4 bg-[#100d21] m-3 w-full md:w-[31%] 2xl:w-[23%] flex flex-col items-center text-white border border-[#ffffff22]'>
        <Avatar 
        src='https://pixner.net/aikeu/assets/images/blog-details/a-one.png'
+       className='w-[80px] h-[80px]'
        />
     </Card>
   )
