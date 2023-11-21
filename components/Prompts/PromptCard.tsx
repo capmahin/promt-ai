6aa3e1a29a4f52be20a8f1732c56a26175b97774
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-21 11:19:08 +0600

    solve the bug

--- components/Prompts/PromptCard.tsx
+++ components/Prompts/PromptCard.tsx
@@ -54,8 +54,8 @@ const PromptCard = (props: Props) => {
       </div>
       <br />
       <Link href='/shop/123' className='w-full'>
-           <Button className={'mb-3 w-full bg-transparent border border-[#16c252] hover:bg-[#16c252] hover:text-black duration-300 transition-opacity font-Inter font-[600]'}>
-              Get Prompt
+           <Button className={'mb-3 w-full text-white bg-transparent border border-[#16c252] hover:bg-[#16c252] hover:text-black duration-300 transition-opacity font-Inter font-[600]'}>
+              Get Prompts
            </Button>
       </Link>
     </Card>
