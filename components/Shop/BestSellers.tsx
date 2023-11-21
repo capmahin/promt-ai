afa6a5a5a89540e5c9b07f8d03b2c8774a9a1bcb
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-21 21:58:56 +0600

    add bestseller and styles it

--- components/Shop/BestSellers.tsx
+++ components/Shop/BestSellers.tsx
@@ -1,10 +1,15 @@
+import { styles } from '@/utils/styles'
 import React from 'react'
 
 type Props = {}
 
 const BestSellers = (props: Props) => {
   return (
-    <div>BestSellers</div>
+    <div className='mb-10 cursor-pointer'>
+        <h1 className={`${styles.heading} p-2 font-Monserrat mb-5`}>
+
+        </h1>
+    </div>
   )
 }
 
