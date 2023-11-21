2f7d88888e2999140d287eb289696e6c6c3c237c
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-21 22:08:59 +0600

    add Avater in card

--- components/Shop/SellerCard.tsx
+++ components/Shop/SellerCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@nextui-org/react'
+import { Avatar, Card } from '@nextui-org/react'
 import React from 'react'
 
 type Props = {}
@@ -6,7 +6,9 @@ type Props = {}
 const SellerCard = (props: Props) => {
   return (
     <Card className='py-4 bg-[#100d21] m-3 w-full md:w-[31%] 2xl:w-[23%] flex flex-col items-center text-white border border-[#ffffff22]'>
-
+       <Avatar 
+       src='https://pixner.net/aikeu/assets/images/blog-details/a-one.png'
+       />
     </Card>
   )
 }
