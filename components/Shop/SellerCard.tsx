f9f9ad6e8f4bb2e1d365498de32298f28303bebe
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-21 22:06:56 +0600

    add another card in sellercard

--- components/Shop/SellerCard.tsx
+++ components/Shop/SellerCard.tsx
@@ -1,10 +1,13 @@
+import { Card } from '@nextui-org/react'
 import React from 'react'
 
 type Props = {}
 
 const SellerCard = (props: Props) => {
   return (
-    <div>SellerCard</div>
+    <Card className='py-4 bg-[#100d21] m-3 w-full md:w-[31%] 2xl:w-[23%] flex flex-col items-center text-white border border-[#ffffff22]'>
+
+    </Card>
   )
 }
 
