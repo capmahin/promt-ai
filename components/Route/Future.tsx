58124264ba0b4324967d416c094af60aafcadaf3
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-22 23:20:24 +0600

    add

--- components/Route/Future.tsx
+++ components/Route/Future.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
@@ -5,7 +6,15 @@ type Props = {}
 const Future = (props: Props) => {
   return (
     <div className='w-full relative p-4 md:p-[unset] grid md:grid-cols-2 py-8'>
-
+       <div className="col-span-1">
+        <Image 
+        src={"https://pixner.net/aikeu/assets/images/tools-thumb.png"}
+        width={800}
+        height={500}
+        alt=''
+        className='md:w-[90%] md:ml-[-50px] 2xl:ml-[-90px]'
+        />
+       </div>
     </div>
   )
 }
