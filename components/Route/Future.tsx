6ad633383e1068791642dbb1ead5788e775b283a
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-22 23:15:20 +0600

    add styles

--- components/Route/Future.tsx
+++ components/Route/Future.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 const Future = (props: Props) => {
   return (
-    <div>Future</div>
+    <div className='w-full relative p-4 md:p-[unset] grid md:grid-cols-2 py-8'>
+
+    </div>
   )
 }
 
