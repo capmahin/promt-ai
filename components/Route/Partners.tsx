78624c4d39b62e252d3ce6b5bd6964e54efa5344
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-24 23:09:33 +0600

    add more partners

--- components/Route/Partners.tsx
+++ components/Route/Partners.tsx
@@ -6,8 +6,8 @@ type Props = {}
 const Partners = (props: Props) => {
   return (
     <div className='py-10'>
-        <h1 className={`${styles.heading}`}>
-
+        <h1 className={`${styles.heading} font-Monserrat text-center`}>
+        Our Partner&apos;s
         </h1>
 
     </div>
