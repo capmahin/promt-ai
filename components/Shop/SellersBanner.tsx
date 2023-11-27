f1b39e51a8606f603f6d1c8ffcc7a51dc0ecdc10
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-28 00:10:07 +0600

    add classname is sellerdanner

--- components/Shop/SellersBanner.tsx
+++ components/Shop/SellersBanner.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 const SellersBanner = (props: Props) => {
   return (
-    <div>SellersBanner</div>
+    <div className='w-full 2xl:w-[80%] 2xl:m-auto h-[30vh] flex items-center justify-center sellers-banner'>
+
+    </div>
   )
 }
 
