cb7ef7334923fc52fe6e7a51a6b25dfd5a9d0dfe
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-29 22:58:45 +0600

    add another h1 with classname

--- components/Shop/SellersBanner.tsx
+++ components/Shop/SellersBanner.tsx
@@ -1,3 +1,4 @@
+import { styles } from '@/utils/styles'
 import React from 'react'
 
 type Props = {}
@@ -6,7 +7,9 @@ const SellersBanner = (props: Props) => {
   return (
     <div className='w-full 2xl:w-[80%] 2xl:m-auto h-[30vh] flex items-center justify-center sellers-banner rounded-xl md:m-2'>
         <div className='text-center'>
+            <h1 className={`${styles.heading} !text-indigo-950 font-Monserrat`}>
 
+            </h1>
         </div>
     </div>
   )
