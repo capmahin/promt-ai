aa73d77a917e99562df0782772db26a320816167
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-29 22:57:17 +0600

    add another div with classname

--- components/Shop/SellersBanner.tsx
+++ components/Shop/SellersBanner.tsx
@@ -5,7 +5,9 @@ type Props = {}
 const SellersBanner = (props: Props) => {
   return (
     <div className='w-full 2xl:w-[80%] 2xl:m-auto h-[30vh] flex items-center justify-center sellers-banner rounded-xl md:m-2'>
+        <div className='text-center'>
 
+        </div>
     </div>
   )
 }
