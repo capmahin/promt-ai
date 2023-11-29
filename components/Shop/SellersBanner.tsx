130ae4044f7854ec5a6c2df97096eb89ee1adaad
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-29 23:00:18 +0600

    add Link

--- components/Shop/SellersBanner.tsx
+++ components/Shop/SellersBanner.tsx
@@ -1,4 +1,5 @@
 import { styles } from '@/utils/styles'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
@@ -8,8 +9,12 @@ const SellersBanner = (props: Props) => {
     <div className='w-full 2xl:w-[80%] 2xl:m-auto h-[30vh] flex items-center justify-center sellers-banner rounded-xl md:m-2'>
         <div className='text-center'>
             <h1 className={`${styles.heading} !text-indigo-950 font-Monserrat`}>
-
+               Start to selling with us
             </h1>
+            <br />
+            <br />
+            <Link href={"/create-shop"}>
+            </Link>
         </div>
     </div>
   )
