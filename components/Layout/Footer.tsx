467c59b644e804c605f52a81f288c4d44e52c69f
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-11-30 23:06:09 +0600

    add another div and classname it

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -5,7 +5,9 @@ type Props = {}
 const Footer = (props: Props) => {
   return (
     <div className='mt-8'>
+      <div className='w-full mb-5 flex justify-between items-center'>
 
+      </div>
     </div>
   )
 }
