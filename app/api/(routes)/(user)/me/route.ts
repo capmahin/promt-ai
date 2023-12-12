699b140773355b9c694af0bd3d5444b733554b9e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-12-12 22:55:55 +0600

    add try catch

--- app/api/(routes)/(user)/me/route.ts
+++ app/api/(routes)/(user)/me/route.ts
@@ -3,5 +3,9 @@ import {User,currentUser} from "@clerk/nextjs/server";
 import {NextRequest,NextResponse} from "next/server";
 
 export async function GET(req:NextRequest){
-    
+    try {
+        
+    } catch (error) {
+        console.log('load user error',error);
+    }
 }
