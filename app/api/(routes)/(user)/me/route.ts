c9d36716a0848ede91769c9f2a4f64c717abecfa
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-12-12 23:00:13 +0600

    working on try

--- app/api/(routes)/(user)/me/route.ts
+++ app/api/(routes)/(user)/me/route.ts
@@ -4,7 +4,7 @@ import {NextRequest,NextResponse} from "next/server";
 
 export async function GET(req:NextRequest){
     try {
-        
+        const user:User | null = await currentUser();
     } catch (error) {
         console.log('load user error',error);
         return new NextResponse("Internal Error",{status: 500})
