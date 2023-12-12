c608e91355a9b08b125a0bf661fc636132eb2077
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-12-12 23:02:10 +0600

    add another response

--- app/api/(routes)/(user)/me/route.ts
+++ app/api/(routes)/(user)/me/route.ts
@@ -5,6 +5,11 @@ import {NextRequest,NextResponse} from "next/server";
 export async function GET(req:NextRequest){
     try {
         const user:User | null = await currentUser();
+        if(!user){
+            return new NextResponse("Please login to access this resources!",{
+                status: 400,
+            });
+        }
     } catch (error) {
         console.log('load user error',error);
         return new NextResponse("Internal Error",{status: 500})
