150553085c81cde23b326ac5d9bdb65bb6e93098
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-12-24 00:09:14 +0600

    add more div and h1

--- app/create-shop/page.tsx
+++ app/create-shop/page.tsx
@@ -5,7 +5,11 @@ type Props = {}
 const Page = (props: Props) => {
   return (
     <div className="w-full h-screen flex flex-col justify-center">
-
+      <div>
+        <h1>
+            Start to selling with us
+        </h1>
+      </div>
     </div>
   )
 }
