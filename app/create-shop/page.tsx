68024fe24e9b907a264bdd799e85ae8d549e2571
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-12-24 00:10:40 +0600

    add classname

--- app/create-shop/page.tsx
+++ app/create-shop/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
+import { styles } from "@/utils/styles";
+
 type Props = {}
 
 const Page = (props: Props) => {
   return (
     <div className="w-full h-screen flex flex-col justify-center">
       <div>
-        <h1>
+        <h1 className={`${styles.heading} text-center font-Monserrat`}>
             Start to selling with us
         </h1>
       </div>
