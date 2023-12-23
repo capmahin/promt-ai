6246291f63e970e7fc72c8180cc5befa0c25febc
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-12-24 00:13:22 +0600

    add another div

--- app/create-shop/page.tsx
+++ app/create-shop/page.tsx
@@ -12,7 +12,9 @@ const Page = (props: Props) => {
             Start to selling with us
         </h1>
         <form className="2xl:w-[40%] xl:w-[50%] md:w-[70%] w-[90%] m-auto">
-            
+            <div className="w-full my-5">
+               
+            </div>
         </form>
       </div>
     </div>
