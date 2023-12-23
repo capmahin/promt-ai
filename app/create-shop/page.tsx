4cdffbd40f19ab6569014bb13836eab93a1d9c0e
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-12-24 00:18:17 +0600

    add lable

--- app/create-shop/page.tsx
+++ app/create-shop/page.tsx
@@ -13,7 +13,9 @@ const Page = (props: Props) => {
         </h1>
         <form className="2xl:w-[40%] xl:w-[50%] md:w-[70%] w-[90%] m-auto">
             <div className="w-full my-5">
-               
+               <label className={`${styles.label}mb-2 block`}>
+                  Shop Name
+               </label>
             </div>
         </form>
       </div>
