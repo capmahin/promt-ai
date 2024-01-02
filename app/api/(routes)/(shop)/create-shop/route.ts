a6784af92a6eaba3ec365d236b2c44606977b205
Hawkeye <36890222+capmahin@users.noreply.github.com>
2024-01-02 21:23:34 +0600

    add more

--- app/api/(routes)/(shop)/create-shop/route.ts
+++ app/api/(routes)/(shop)/create-shop/route.ts
@@ -3,7 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req:NextRequest){
     try {
-        
+        const data = await req.json();
+
+        const userId = data.userId;
+
+        //check if user with given userId exists
+        const user = await prisma.shops.findUnique({
+            where:{
+                userId
+            }
+        })
     } catch (error) {
        console.log('create shop error',error);
        return new NextResponse("Internal Error",{status:500}) 
