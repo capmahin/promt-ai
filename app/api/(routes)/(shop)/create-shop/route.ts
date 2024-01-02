a2046d0ee4d8e38e7fd850f8acb1c797996f0052
Hawkeye <36890222+capmahin@users.noreply.github.com>
2024-01-02 12:33:03 +0600

    add try catch block

---
+++ app/api/(routes)/(shop)/create-shop/route.ts
@@ -0,0 +1,10 @@
+import prisma from "@/lib/prismaDb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
