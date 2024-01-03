fab3ec8612fa5221b4966e1a984d29f5f59d6020
Hawkeye <36890222+capmahin@users.noreply.github.com>
2024-01-03 20:57:40 +0600

    add more option in array

--- components/Shop/ShopSidebar.tsx
+++ components/Shop/ShopSidebar.tsx
@@ -1,5 +1,10 @@
 import React from 'react' 
-import {GoHome} from 'react-icons/go'
+import {GoHome, GoArrowSwitch} from 'react-icons/go'
+import { MdOutlineCreateNewFolder } from "react-icons/md";
+import { BsWallet2 } from "react-icons/bs";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
+import { TbMoneybag } from "react-icons/tb";
+import { BiMoneyWithdraw } from "react-icons/bi";
 
 type Props = {
     active: number;
@@ -7,8 +12,40 @@ type Props = {
 
 const sideBarItems=[
     {
-        icon: <GoHome/>
-    }
+        icon: <GoHome/>,
+        title:"Dashboard",
+        href:"/my-shop"
+    },
+    {
+        icon: <MdOutlineCreateNewFolder />,
+        title: "Upload Prompt",
+        href:"/shop/create-prompt",
+    },
+    {
+        icon: <BsWallet2 />,
+        title: "Prompts",
+        href: "/shop/prompts",
+      },
+      {
+        icon: <TbMoneybag />,
+        title: "Orders",
+        href: "/shop/orders",
+      },
+      {
+        icon: <LiaFileInvoiceDollarSolid />,
+        title: "Invoices",
+        href: "/shop/invoices",
+      },
+      {
+        icon: <BiMoneyWithdraw />,
+        title: "Withdraw Earnings",
+        href: "/shop/withdraw",
+      },
+      {
+        icon: <GoArrowSwitch />,
+        title: "Switch to user",
+        href: "/",
+      },
 ]
 const ShopSidebar = ({active}: Props) => {
   return (
