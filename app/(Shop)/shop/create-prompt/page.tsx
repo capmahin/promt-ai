4513c31b5e4ce5c6a943dcf315e01b4ffb8b7083
Hawkeye <36890222+capmahin@users.noreply.github.com>
2024-01-11 01:54:56 +0600

    add more options

--- app/(Shop)/shop/create-prompt/page.tsx
+++ app/(Shop)/shop/create-prompt/page.tsx
@@ -7,7 +7,7 @@ const Page = (props: Props) => {
   return (
     <div className="flex w-full">
             <div className="h-screen flex p-2 bg-[#111C42] md:w-[20%] 2xl:w-[17%]">
-                <ShopSidebar active={0}/>   
+                <ShopSidebar active={1}/>   
             </div>
             <div className="md:w-[80%] 2xl:w-[83%]">
                   
